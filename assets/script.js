7e8622d2efeca22c5daed6957354f06afffe42ec
Gise Haag <ing.giselahaag@gmail.com>
2021-02-08 11:32:41 +0100

    Add moreInfo button

--- assets/script.js
+++ assets/script.js
@@ -63,6 +63,8 @@ class MediaPlayer {
 		this.progressRange = this.controls.querySelector('.controls__progress-bar');
 		this.displayVideoDuration = this.controls.querySelector('.duration');
 		this.displayProgress = this.controls.querySelector('.progress');
+
+		this.moreInfoButton = document.querySelector('#more-info');
 	}
 
 	addEvents() {
@@ -79,6 +81,36 @@ class MediaPlayer {
 		this.backwardForwardButtons.forEach((button) => {
 			button.addEventListener('click', this.backwardForwardHandler.bind(this));
 		});
+
+		this.displayMoreInfo = this.displayMoreInfo.bind(this);
+		this.moreInfoButton.addEventListener('click', this.displayMoreInfo);
+	}
+
+	displayMoreInfo() {
+		let infoBox = document.querySelector('.text-box');
+		let moreInfo = document.createElement('div');
+
+		moreInfo.innerHTML = `
+			<p class="text">
+				Motivada por mis ganas de practicar un poco más JavaScript, e inspirada por el proyecto que planteaba el
+				curso profesional de JS de Platzi, se me ocurrió hacer mi propio reproductor de videos. Claramente hay miles
+				de reproductores, pero porqué no hacer uno con mi impronta?
+			</p>
+			<p class="text">
+				A medida que lo fui desarrollando, se me ocurrio hacerlo como si fuera un pluggin que cualquier
+				desarrolladora o desarrollador pueda usar en su proyecto. Esto me planteo distintos desafios que fui
+				resolviendo con entuasiamo.
+			</p>
+			<p class="text">
+				El script crea el template, sólo brindando una url que contiene el video y un elemento dónde alojarlo, junto
+				con sus controles, el pluggins se encarga del resto. Asimismo, la clase puede tener varias instancias (con
+				distintos elementos de configuración), si se quiere tener varios videos, y obviamente cada uno con sus
+				controles que funcionan de forma independiente.
+			</p>
+		`
+		infoBox.appendChild(moreInfo);
+
+		this.moreInfoButton.removeEventListener('click', this.displayMoreInfo);
 
 	}
 
